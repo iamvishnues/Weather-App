0031891e85160944cf222150dba77b9c6d6a2e24
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2022-07-22 23:25:54 +0530

    Animating Splash Screen

--- app/src/main/java/com/esvisoftech/weatherapp/screens/splash/WeatherSplashScreen.kt
+++ app/src/main/java/com/esvisoftech/weatherapp/screens/splash/WeatherSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.esvisoftech.weatherapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -8,23 +11,40 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.esvisoftech.weatherapp.R
+import com.esvisoftech.weatherapp.navigation.WeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun WeatherSplashScreen(navController:NavController) {
+    val scale=remember{
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    //Animation
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(targetValue = 0.9f, animationSpec = tween(800,
+        easing = {
+            OvershootInterpolator(8f).getInterpolation(it)
+        }))
+        delay(2000L)
+        navController.navigate(WeatherScreens.MainScreen.name)
+    })
+
     androidx.compose.material.Surface(modifier = Modifier
         .padding(15.dp)
         .size(330.dp)
-//        , shape= CircleShape,color= Color.White,
-//    border = BorderStroke(width = 2.dp,color=Color.LightGray
-//    )
+        .scale(scale.value)
     ) {
 Column(modifier = Modifier.padding(1.dp),
 horizontalAlignment = Alignment.CenterHorizontally,
