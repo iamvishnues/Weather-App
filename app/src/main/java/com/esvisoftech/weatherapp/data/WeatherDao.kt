3cbc2493bcc13c709f621a140d20a2000722b29d
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2022-08-04 23:35:26 +0530

    Implementing Entity & DAO

---
+++ app/src/main/java/com/esvisoftech/weatherapp/data/WeatherDao.kt
@@ -0,0 +1,27 @@
+package com.esvisoftech.weatherapp.data
+
+import androidx.room.*
+import com.esvisoftech.weatherapp.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface WeatherDao {
+    @Query("SELECT * FROM fav_tbl")
+    fun getFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * FROM fav_tbl WHERE city=:city")
+    suspend fun getFavById(city:String):Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite)
+
+    @Query("DELETE FROM fav_tbl")
+    suspend fun deleteAllFavorites()
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
+}
