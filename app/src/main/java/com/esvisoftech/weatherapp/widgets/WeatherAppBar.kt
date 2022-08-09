b6019ddf3a0d135c16f00754d854cbf6974d1788
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2022-08-09 22:44:11 +0530

    display toast

--- app/src/main/java/com/esvisoftech/weatherapp/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/esvisoftech/weatherapp/widgets/WeatherAppBar.kt
@@ -1,5 +1,7 @@
 package com.esvisoftech.weatherapp.widgets
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
@@ -37,6 +40,12 @@ fun WeatherAppBar(
     val showDialog=remember{
   mutableStateOf(false)
     }
+
+    val showIt = remember {
+        mutableStateOf(false)
+    }
+    val context= LocalContext.current
+
     if(showDialog.value){
  ShowSettingDropDown(showDialog=showDialog,navController=navController)
     }
@@ -73,21 +82,47 @@ showDialog.value=true
                                onButtonClicked.invoke()
                            })
                        }
-            if(isMainScreen){
-                val dataList=title.split(",")
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "Favorite Icons", tint = Color.Red.copy(alpha = 0.6f)
-                ,modifier=Modifier.scale(0.9f).clickable {
-                    favoriteViewModel.insertFavorite(Favorite(city =dataList[0],
-                        country =dataList[1] ))
-                    }
-                )
 
+            if(isMainScreen){
+                val isAlreadyfavList=favoriteViewModel.favList.collectAsState().value.filter {
+                    item->(item.city==title.split(",")[0])
+                }
+                val dataList=title.split(",")
+                if(isAlreadyfavList.isNullOrEmpty()){
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Favorite Icons", tint = Color.Red.copy(alpha = 0.6f)
+                        ,modifier= Modifier
+                            .scale(0.9f)
+                            .clickable {
+                                favoriteViewModel.insertFavorite(
+                                    Favorite(
+                                        city = dataList[0],
+                                        country = dataList[1]
+                                    )
+                                ).run {
+                                    showIt.value=true
+                                }
+                            }
+                    )
+                }
+               else{
+                   showIt.value=false
+                   Box{}
+                }
+                ShowToast(context = context,showIt)
             }
 
         },
     backgroundColor = Color.Transparent, elevation = elevation)
+}
+
+
+@Composable
+fun ShowToast(context: Context, showIt: MutableState<Boolean>) {
+if (showIt.value){
+    Toast.makeText(context,"Added to Favorites",Toast.LENGTH_SHORT)
+}
 }
 
 @Composable
